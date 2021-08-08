e8c1ccf20fe144e9d1361da8097227ea23769a04
rahul shukla <rahul.shukla754@gmail.com>
2021-08-08 14:03:58 +0530

    feat: add mobile responsive

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+/* mobile js */
+const lineMobile = document.querySelector('.mobile-menu');
+const mainMenu  = document.querySelector('#main_menu');
+const linkName  = document.querySelector('.list-item-inline');
+
+function showMenu(){
+    mainMenu.style.display = "block";
+}
+
+function hideMenu(){
+    mainMenu.style.display = "none";
+}
+
+lineMobile.addEventListener("mouseover",showMenu,false); 
+linkName.addEventListener('click',hideMenu,false);
